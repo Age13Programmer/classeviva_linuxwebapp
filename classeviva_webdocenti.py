00733d4c3b6587d112fc86957e84c6b2b6fbfa7c
Age13Programmer <132699533+Age13Programmer@users.noreply.github.com>
2025-09-19 15:26:25 +0200

    upload files

---
+++ classeviva_webdocenti.py
@@ -0,0 +1,5 @@
+import webview
+
+# finestra massimizzata
+webview.create_window("Classeviva Web Docenti", "https://web.spaggiari.eu/home/app/default/login.php?target=cvv", resizable=True)
+webview.start()
